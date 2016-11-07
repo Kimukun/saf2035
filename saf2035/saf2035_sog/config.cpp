221e82a4ae5831486ebe6c414d1a8edb8a1fb14e
Kimukun <info@kimu.se>
2016-11-07 23:39:12 +0100

    161107_1
    SOG Change
    Change uniform
    Change camopattern

--- saf2035/saf2035_sog/config.cpp
+++ saf2035/saf2035_sog/config.cpp
@@ -1,34 +1,36 @@
 class CfgPatches {
   class SAF_SOG_CONFIG {
     units[] = {
-      "SAF_Scorpion_Soldier_base",
-      "SAF_SOG_Diver_base",
-      "SAF_Scorpion_SOG_SAT",
-			"SAF_Scorpion_SOG_SDemo",
-			"SAF_Scorpion_SOG_SMM",
-			"SAF_Scorpion_SOG_SME",
-			"SAF_Scorpion_SOG_STL",
-			"SAF_Scorpion_SOG_SMG",
-      "SAF_SOG_Diver_STL",
-      "SAF_SOG_Diver_SDemo",
-      "SAF_SOG_Diver_SME",
-      "SAF_SOG_Diver_SMG",
-      "SAF_Scorpion_Kitbag_base",
-      "SAF_Scorpion_CompactBag_base",
-      "SAF_Scorpion_CompactBag_OP",
-			"SAF_Scorpion_CompactBag_MM",
-			"SAF_Scorpion_Kitbag_TL",
-			"SAF_Scorpion_Kitbag_MG",
+      "SAF_MC_Soldier_base",
+      "SAF_SOG_DDiver_base",
+      "SAF_SOG_ADiver_base",
+      "SAF_MC_SOG_SAT",
+			"SAF_MC_SOG_SDemo",
+			"SAF_MC_SOG_SMM",
+			"SAF_MC_SOG_SME",
+			"SAF_MC_SOG_STL",
+			"SAF_MC_SOG_SMG",
+      "SAF_SOG_DDiver_STL",
+      "SAF_SOG_DDiver_SDemo",
+      "SAF_SOG_DDiver_SME",
+      "SAF_SOG_DDiver_SMG",
+      "SAF_MC_Kitbag_base",
+      "SAF_MC_CompactBag_base",
+      "SAF_MC_CompactBag_OP",
+			"SAF_MC_CompactBag_MM",
+			"SAF_MC_Kitbag_TL",
+			"SAF_MC_Kitbag_MG",
       "SAF_VH_Blk_S",
       "SAF_VH_Blk_SMG"
     };
     weapons[] = {
-      "SAF_Scorpion_Uniform",
-      "SAF_Scorpion_PC",
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scorpion_PC_spec",
-      "SAF_Scoprion_Helmet_ECH_lite",
-			"SAF_Scoprion_Boonie"
+      "SAF_MC_Uniform",
+      "SAF_MC_Swim_Uniform",
+      "SAF_MC_PC",
+			"SAF_MC_PC_lite",
+			"SAF_MC_PC_spec",
+      "SAF_MC_Helmet_ECH_lite",
+			"SAF_MC_Boonie"
     };
     magazines[] = {};
 		ammo[] = {};
@@ -48,7 +50,7 @@ class CfgPatches {
 class CfgVehicles {
   //Base Soldiers config
   class B_Soldier_base_F;
-  class SAF_Scorpion_C_Soldier_base: B_Soldier_base_F {
+  class SAF_MC_C_Soldier_base: B_Soldier_base_F {
 		author = "Kimukun";
 		backpack = "";
 		camouflage = 0.8;
@@ -60,7 +62,7 @@ class CfgVehicles {
 		engieneer = 0;
 		faction = "SAF";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\saf2035_sog\data\uniform\scorpion_cyre.paa"};
+		hiddenSelectionsTextures[] = {"\saf2035_sog\data\uniform\mc_cyre.paa"};
 		icon="iconMan";
 		identityTypes[] = {"LanguageENG_F","Head_NATO","G_NATO_default"};
 		items[] = {};
@@ -81,10 +83,10 @@ class CfgVehicles {
 		textSingular = "infantry";
 		threat[] = {1,0.1,0.1};
 		uavHacker = 0;
-		uniformClass = "SAF_Scorpion_Uniform";
+		uniformClass = "SAF_MC_Uniform";
 		weapons[] = {"Throw","Put"};
 	};
-  class SAF_SOG_Diver_base: B_Soldier_base_F {
+  class SAF_SOG_DDiver_base: B_Soldier_base_F {
     author = "Kimukun";
 		backpack = "";
 		camouflage = 0.8;
@@ -123,13 +125,53 @@ class CfgVehicles {
 		uniformClass = "U_B_Wetsuit";
 		weapons[] = {"Throw","Put"};
   };
+  class SAF_SOG_ADiver_base: B_Soldier_base_F {
+    author = "Kimukun";
+    backpack = "";
+    camouflage = 0.8;
+    canDeactivateMines = 0;
+    cost = 100000;
+    detectSkill = 20;
+    displayName = "Should not Show in Editor";
+    editorSubcategory = "EdSubcat_Personnel_SpecialForces";
+    engieneer = 0;
+    faction = "SAF";
+    hiddenSelections[] = {"Camo","insignia"};
+    hiddenSelectionsMaterials[] = {};
+    hiddenSelectionsTextures[] = {"\a3\characters_f\blufor\data\clothing1_co.paa"};
+    hiddenUnderwaterSelections[] = {"hide"};
+    hiddenUnderwaterSelectionsTextures[] = {"\A3\characters_f\common\data\diver_equip_nato_co.paa","\A3\characters_f\common\data\diver_equip_nato_co.paa","\A3\characters_f\data\visors_ca.paa"};
+    icon="iconMan";
+    identityTypes[] = {"LanguageENG_F","Head_NATO","G_NATO_default"};
+    items[] = {};
+    linkedItems[] = {};
+    magazines[] = {};
+    model="\A3\Characters_F_EPA\BLUFOR\b_soldier_survival_01.p3d";
+    respawnItems[] = {};
+    respawnLinkedItems[] = {};
+    respawnMagazines[] = {};
+    respawnWeapons[] = {"Throw","Put"};
+    role = "Rifleman";
+    scope = 1;
+    scopeCurator = 0;
+    scopeArsenal = 0;
+    sensitivity = 3;
+    shownUnderwaterSelections[] = {"unhide","unhide2"};
+    side = 1;
+    textPlural = "divers";
+    textSingular = "diver";
+    threat[] = {1,0.1,0.1};
+    uavHacker = 0;
+    uniformClass = "";
+    weapons[] = {"Throw","Put"};
+  };
   // SOG Soldier Config
-  class SAF_Scorpion_SOG_SAT: SAF_Scorpion_Soldier_base {
-		backpack = "SAF_Scorpion_CompactBag_OP";
+  class SAF_MC_SOG_SAT: SAF_MC_Soldier_base {
+		backpack = "SAF_MC_CompactBag_OP";
 		displayName = "Specialist";
     linkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -163,8 +205,8 @@ class CfgVehicles {
 			"Chemlight_green"
 		};
 		respawnLinkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -217,15 +259,15 @@ class CfgVehicles {
 			"Put"
 		};
 	};
-	class SAF_Scorpion_SOG_SDemo: SAF_Scorpion_Soldier_base {
-		backpack = "SAF_Scorpion_Kitbag_base";
+	class SAF_MC_SOG_SDemo: SAF_MC_Soldier_base {
+		backpack = "SAF_MC_Kitbag_base";
 		displayName = "Specialist (Demo)";
 		class eventHandlers {
 			init = "(_this select 0) execVM '\saf2035_sog\scripts\sogexpbags.sqf';";
 		};
 		linkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -258,8 +300,8 @@ class CfgVehicles {
 			"Chemlight_green"
 		};
 		respawnLinkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -309,12 +351,12 @@ class CfgVehicles {
 			"Put"
 		};
 	};
-	class SAF_Scorpion_SOG_SMM: SAF_Scorpion_C_Soldier_base {
-		backpack = "SAF_Scorpion_CompactBag_MM";
+	class SAF_MC_SOG_SMM: SAF_MC_C_Soldier_base {
+		backpack = "SAF_MC_CompactBag_MM";
 		displayName = "Specialist (Marksman)";
 		linkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -346,8 +388,8 @@ class CfgVehicles {
 			"Chemlight_green"
 		};
 		respawnLinkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -396,15 +438,15 @@ class CfgVehicles {
 			"Put"
 		};
 	};
-	class SAF_Scorpion_SOG_SME: SAF_Scorpion_Soldier_base {
-		backpack = "SAF_Scorpion_Kitbag_base";
+	class SAF_MC_SOG_SME: SAF_MC_Soldier_base {
+		backpack = "SAF_MC_Kitbag_base";
 		displayName = "Specialist (Medic)";
 		class eventHandlers {
 			init = "(_this select 0) execVM '\saf2035_sog\scripts\medicbags.sqf';";
 		};
 		linkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -437,8 +479,8 @@ class CfgVehicles {
 			"Chemlight_green"
 		};
 		respawnLinkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -488,15 +530,15 @@ class CfgVehicles {
 			"Put"
 		};
 	};
-	class SAF_Scorpion_SOG_STL: SAF_Scorpion_Soldier_base {
-		backpack = "SAF_Scorpion_Kitbag_TL";
+	class SAF_MC_SOG_STL: SAF_MC_Soldier_base {
+		backpack = "SAF_MC_Kitbag_TL";
 		displayName="Specialist (Team Leader)";
 		class eventHandlers {
 			init = "(_this select 0) execVM '\saf2035\scripts\sogtlbags.sqf';";
 		};
 		linkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -529,8 +571,8 @@ class CfgVehicles {
 			"Chemlight_green"
 		};
 		respawnLinkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -580,12 +622,12 @@ class CfgVehicles {
 			"Put"
 		};
 	};
-	class SAF_Scorpion_SOG_MG: SAF_Scorpion_Soldier_base {
-		backpack = "SAF_Scorpion_Kitbag_MG";
+	class SAF_MC_SOG_MG: SAF_MC_Soldier_base {
+		backpack = "SAF_MC_Kitbag_MG";
 		displayName = "Specialist (MG)";
 		linkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -612,8 +654,8 @@ class CfgVehicles {
 			"Chemlight_green"
 		};
 		respawnLinkedItems[] = {
-			"SAF_Scorpion_PC_lite",
-			"SAF_Scoprion_Helmet_ECH_lite",
+			"SAF_MC_PC_lite",
+			"SAF_MC_Helmet_ECH_lite",
 			"G_Bandanna_oli",
 			"ItemMap",
 			"ItemCompass",
@@ -657,7 +699,7 @@ class CfgVehicles {
 			"Put"
 		};
 	};
-  class SAF_SOG_Diver_STL: SAF_SOG_Diver_base {
+  class SAF_SOG_DDiver_STL: SAF_SOG_Diver_base {
     backpack = "SAF_VH_Blk_S";
 		displayName="Specialist (Team Leader)";
 		class eventHandlers {
@@ -733,31 +775,31 @@ class CfgVehicles {
   class B_Kitbag_Base;
 	class B_AssaultPack_Base;
   class B_ViperHarness_base_F;
-  class SAF_Scorpion_Kitbag_base: B_Kitbag_Base {
+  class SAF_MC_Kitbag_base: B_Kitbag_Base {
 		author = "Kimukun";
-		baseBackpack = "SAF_Scorpion_Kitbag_base";
-		displayName = "Scorpion W2 Kitbag";
+		baseBackpack = "SAF_MC_Kitbag_base";
+		displayName = "MC W2 Kitbag";
 		dlc = "SAF2035_Mod";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\saf2035_sog\data\backpack\scorpion_kitbag.paa"};
+		hiddenSelectionsTextures[] = {"\saf2035_sog\data\backpack\mc_kitbag.paa"};
 		picture = "\A3\weapons_f\ammoboxes\bags\data\ui\icon_b_c_kitbag_rgr.paa";
 		scope = 2;
 		scopeCurator = 2;
 		scopeArsenal = 2;
 	};
-	class SAF_Scorpion_CompactBag_base: B_AssaultPack_Base {
+	class SAF_MC_CompactBag_base: B_AssaultPack_Base {
 		author = "Kimukun";
-		baseBackpack = "SAF_Scorpion_CompactBag_base";
-		displayName = "Scorpion W2 Compact Bag";
+		baseBackpack = "SAF_MC_CompactBag_base";
+		displayName = "MC W2 Compact Bag";
 		dlc = "SAF2035_Mod";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\saf2035_sog\data\backpack\scorpion_compact.paa"};
+		hiddenSelectionsTextures[] = {"\saf2035_sog\data\backpack\mc_compact.paa"};
 		picture = "\A3\weapons_f\ammoboxes\bags\data\ui\icon_b_c_kitbag_rgr.paa";
 		scope = 2;
 		scopeCurator = 2;
 		scopeArsenal = 2;
 	};
-  class SAF_Scorpion_CompactBag_OP: SAF_Scorpion_CompactBag_base {
+  class SAF_MC_CompactBag_OP: SAF_MC_CompactBag_base {
 		scope = 1;
 		scopeArsenal = 0;
 		scopeCurator = 0;
@@ -768,7 +810,7 @@ class CfgVehicles {
 			};
 		};
 	};
-	class SAF_Scorpion_CompactBag_MM: SAF_Scorpion_CompactBag_base {
+	class SAF_MC_CompactBag_MM: SAF_MC_CompactBag_base {
 		scope = 1;
 		scopeArsenal = 0;
 		scopeCurator = 0;
@@ -779,7 +821,7 @@ class CfgVehicles {
 			};
 		};
 	};
-	class SAF_Scorpion_Kitbag_TL: SAF_Scorpion_Kitbag_base {
+	class SAF_MC_Kitbag_TL: SAF_MC_Kitbag_base {
 		scope = 1;
 		scopeArsenal = 0;
 		scopeCurator = 0;
@@ -794,7 +836,7 @@ class CfgVehicles {
 			};
 		};
 	};
-	class SAF_Scorpion_Kitbag_MG: SAF_Scorpion_Kitbag_base {
+	class SAF_MC_Kitbag_MG: SAF_MC_Kitbag_base {
 		scope = 1;
 		scopeArsenal = 0;
 		scopeCurator = 0;
@@ -832,30 +874,49 @@ class cfgWeapons {
   // Uniforms
   class UniformItem;
 	class Uniform_Base;
-  class SAF_Scorpion_Uniform: Uniform_Base {
+  class SAF_MC_Uniform: Uniform_Base {
 		author = "Kimukun";
 		scope = 2;
-		displayName = "Scorpion W2 (Cyre)";
+		displayName = "Multicam (Crye)";
 		dlc = "SAF2035_Mod";
 		picture = "\A3\characters_f\Data\UI\icon_f_b_combatuniform_ocam_ca.paa";
 		model = "\A3\Characters_F\BLUFOR\b_soldier_01.p3d";
 		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"\saf2035_sog\data\uniform\scorpion_cyre.paa"};
+		hiddenSelectionsTextures[] = {"\saf2035_sog\data\uniform\mc_cyre.paa"};
 		class ItemInfo: UniformItem {
-			uniformClass = "SAF_Scorpion_C_Soldier_base";
+			uniformClass = "SAF_MC_C_Soldier_base";
 			containerClass = "Supply40";
 			mass = 40;
+		};
+	};
+  class SAF_MC_ADiver_Uniform: Uniform_Base {
+		author = "Kimukun";
+		scope = 2;
+		displayName = "Multicam Diver (Crye)";
+		dlc = "SAF2035_Mod";
+		picture = "\saf2035_m90\data\ui\uniform_m90_icon.paa";
+		model = "\A3\characters_f_beta\INDEP\ia_soldier_01.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"\saf2035_m90\data\uniform\m90_uniform.paa"};
+		hiddenUnderwaterSelections[] = {};
+		hiddenUnderwaterSelectionsTextures[] = {};
+		shownUnderwaterSelections[] = {};
+		class ItemInfo: UniformItem {
+			uniformClass = "SAF_M90_Soldier_base";
+			containerClass = "Supply40";
+			mass = 40;
+			uniformType = "Neopren";
 		};
 	};
   // Vests
   class VestItem;
 	class V_PlateCarrier1_rgr;
-  class SAF_Scorpion_PC: V_PlateCarrier1_rgr {
+  class SAF_MC_PC: V_PlateCarrier1_rgr {
 		author = "Kimukun";
-		displayName = "Scorpion W2 Platecarrier";
+		displayName = "Multicam Platecarrier";
 		dlc = "SAF2035_Mod";
 		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"\saf2035\data\vest\scorpion_vest.paa"};
+		hiddenSelectionsTextures[] = {"\saf2035\data\vest\mc_vest.paa"};
 		class ItemInfo: VestItem {
 			_generalMacro = "VestItem";
 			author = "Kimukun";
@@ -891,12 +952,12 @@ class cfgWeapons {
 			};
 		};
 	};
-	class SAF_Scorpion_PC_lite: V_PlateCarrier1_rgr {
+	class SAF_MC_PC_lite: V_PlateCarrier1_rgr {
 		author = "Kimukun";
-		displayName = "Scorpion W2 Platecarrier Lite";
+		displayName = "Multicam Platecarrier Lite";
 		dlc = "SAF2035_Mod";
 		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"\saf2035\data\vest\scorpion_vest.paa"};
+		hiddenSelectionsTextures[] = {"\saf2035\data\vest\mc_vest.paa"};
 		class ItemInfo: VestItem {
 			_generalMacro = "VestItem";
 			author = "Kimukun";
@@ -932,13 +993,13 @@ class cfgWeapons {
 			};
 		};
 	};
-	class SAF_Scorpion_PC_spec: V_PlateCarrier1_rgr
+	class SAF_MC_PC_spec: V_PlateCarrier1_rgr
 	{
 		author = "Kimukun";
-		displayName = "Scorpion W2 Platecarrier Special";
+		displayName = "Multicam Platecarrier Special";
 		dlc = "SAF2035_Mod";
 		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"\saf2035\data\vest\scorpion_vest.paa"};
+		hiddenSelectionsTextures[] = {"\saf2035\data\vest\mc_vest.paa"};
 		class ItemInfo: VestItem {
 			_generalMacro = "VestItem";
 			author = "Kimukun";
@@ -977,15 +1038,15 @@ class cfgWeapons {
   //Headgear
 	class H_HelmetB;
 	class HeadgearItem;
-  class SAF_Scoprion_Helmet_ECH_lite: H_HelmetB
+  class SAF_MC_Helmet_ECH_lite: H_HelmetB
 	{
 		author = "Kimukun";
-		displayName = "Scorpion W2 ECH Lite";
+		displayName = "Multicam ECH Lite";
 		dlc = "SAF2035_Mod";
 		picture = "\saf2035\data\ui\echM90.paa";
 		model = "A3\Characters_F\BLUFOR\headgear_b_helmet_light";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\saf2035\data\headgear\scorpion_helmet.paa"};
+		hiddenSelectionsTextures[] = {"\saf2035\data\headgear\mc_helmet.paa"};
 		class ItemInfo: HeadgearItem {
 			mass = 20;
 			allowedSlots[] = {901,605};
@@ -1001,14 +1062,14 @@ class cfgWeapons {
 			};
 		};
 	};
-	class SAF_Scoprion_Boonie: H_HelmetB {
+	class SAF_MC_Boonie: H_HelmetB {
 		author = "Kimukun";
-		displayName = "Scorpion W2 Boonie";
+		displayName = "Multicam Boonie";
 		dlc = "SAF2035_Mod";
 		picture = "\saf2035\data\ui\boonieMC.paa";
 		model = "A3\Characters_F\Common\booniehat";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\saf2035\data\headgear\scorpion_boonie.paa"};
+		hiddenSelectionsTextures[] = {"\saf2035\data\headgear\mc_boonie.paa"};
 		class ItemInfo: HeadgearItem {
 			mass = 20;
 			allowedSlots[] = {901,605};
@@ -1038,37 +1099,37 @@ class cfgGroups {
           side = 1;
           class Unit0 {
             side = 1;
-            vehicle = "SAF_Scorpion_SOG_STL";
+            vehicle = "SAF_MC_SOG_STL";
             rank = "CAPTAIN";
             position[] = {0,0,0};
           };
           class Unit1 {
             side = 1;
-            vehicle = "SAF_Scorpion_SOG_SME";
+            vehicle = "SAF_MC_SOG_SME";
             rank = "LIEUTENANT";
             position[] = {5,-5,0};
           };
           class Unit2 {
             side = 1;
-            vehicle = "SAF_Scorpion_SOG_SAT";
+            vehicle = "SAF_MC_SOG_SAT";
             rank = "SERGEANT";
             position[] = {-5,-5,0};
           };
           class Unit3 {
             side = 1;
-            vehicle = "SAF_Scorpion_SOG_SME";
+            vehicle = "SAF_MC_SOG_SME";
             rank = "SERGEANT";
             position[] = {10,-10,0};
           };
           class Unit4 {
             side = 1;
-            vehicle = "SAF_Scorpion_SOG_SAT";
+            vehicle = "SAF_MC_SOG_SAT";
             rank = "SERGEANT";
             position[] = {-10,-10,0};
           };
           class Unit5 {
             side = 1;
-            vehicle = "SAF_Scorpion_SOG_SMG";
+            vehicle = "SAF_MC_SOG_SMG";
             rank = "SERGEANT";
             position[] = {15,-15,0};
           };
